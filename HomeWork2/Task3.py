55fa9ac462cb63b242b03c9628bd7d28b61ddee2
Yuliya Borovaya <Liana-rus81@mail.ru>
2022-10-14 15:17:52 +0300

    Домашняя работа 2

---
+++ HomeWork2/Task3.py
@@ -0,0 +1,11 @@
+#3. Задайте список из n чисел, заполненный по формуле (1 + 1/n) ** n и выведите на экран их сумму.
+#Для n = 6: [2, 2, 2, 2, 2, 3] -> 13
+
+a = int(input())
+list_number = []
+sum = 0
+for i in range(1, a+1):
+    result = round((1+1/i)**i)
+    list_number.append(result)
+    sum += result
+print(list_number, sum)
